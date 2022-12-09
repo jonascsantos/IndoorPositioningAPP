dbe6428087ba866fb5051a8a46b3085ff5cc5f6a
Jonas dos Santos <jonas.cassiano@hotmail.com>
2022-12-09 11:34:15 +0100

    APP | Firebase Config File - API KEY through NODE_ENV

---
+++ src/utils/firebase.js
@@ -0,0 +1,28 @@
+
+import firebase from 'firebase'
+import "firebase/storage";
+
+export const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: 'flowsensor-bfbed.firebaseapp.com',
+  databaseURL: 'https://flowsensor-bfbed.firebaseio.com',
+  projectId: 'flowsensor-bfbed',
+  storageBucket: 'flowsensor-bfbed.appspot.com',
+  messagingSenderId: '529363595938',
+  appId: "1:529363595938:web:25060447cf953e866c1dfb"
+}
+
+let firebaseApp
+
+if (firebase.apps && firebase.apps.length > 0) {
+  firebaseApp = firebase.apps[0]
+} else {
+  firebaseApp = firebase.initializeApp(config)
+}
+
+export default firebaseApp 
+
+
+
+
+
